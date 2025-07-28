e42496e2af0f5cecf29b756868585519557bf5b6
AdriGit97 <60340269+AdriGit97@users.noreply.github.com>
2025-07-28 13:39:36 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -131,3 +131,122 @@ METHOD onactiongrabar .
 
   " Volver a la vista principal
   wd_this->fire_out_vista_sec_back_plg( ).
+--
+METHOD wddoinit .
+
+  DATA: lo_cmp_usage       TYPE REF TO if_wd_component_usage,
+        lo_salv_wd_table   TYPE REF TO iwci_salv_wd_table,
+        lo_column_settings TYPE REF TO if_salv_wd_column_settings,
+        lo_column          TYPE REF TO cl_salv_wd_column,
+        lt_column          TYPE salv_wd_t_column_ref,
+        ls_column          TYPE salv_wd_s_column_ref,
+        lo_column_header   TYPE REF TO cl_salv_wd_column_header,
+        lr_config          TYPE REF TO cl_salv_wd_config_table,
+        l_table            TYPE salv_t_column_ref.
+
+*    Creamos el componente ALV
+  lo_cmp_usage = wd_this->wd_cpuse_alv_principal( ).
+
+  " Si no está creado, lo creamos
+  IF lo_cmp_usage->has_active_component( ) IS INITIAL.
+    lo_cmp_usage->create_component( ).
+  ENDIF.
+
+  " Recuperamos el componente ALV
+  lo_salv_wd_table = wd_this->wd_cpifc_alv_principal( ).
+  lr_config = lo_salv_wd_table->get_model( ).
+  lr_config->if_salv_wd_table_settings~set_cell_action_event_enabled( abap_true ).
+  lo_salv_wd_table->get_model( )->if_salv_wd_table_settings~set_visible_row_count( '30' ).
+  lo_column_settings ?= lo_salv_wd_table->get_model( ).
+
+  " Recuperamos las columnas
+  lt_column = lo_column_settings->get_columns( ).
+
+  LOOP AT lt_column INTO ls_column.
+    lo_column = lr_config->if_salv_wd_column_settings~get_column( id = ls_column-id ).
+    CASE ls_column-id.
+
+        " Configurar todos los campos del ALV
+      WHEN 'MANDT'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_none ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Mandante' ).
+
+      WHEN 'CARRID'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Compañia aérea' ).
+
+      WHEN 'CONNID'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Cod. conexión de vuelo directo' ).
+
+      WHEN 'FLDATE'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Fecha de vuelo' ).
+
+      WHEN 'PRICE'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Precio del vuelo' ).
+
+      WHEN 'CURRENCY'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Moneda local de la compañía aérea' ).
+
+      WHEN 'PLANETYPE'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Tipo de avión' ).
+
+      WHEN 'SEATSMAX'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Ocupación máxima en clase económica' ).
+      WHEN 'SEATSOCC'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Plazas ocupadas en clase económica' ).
+      WHEN 'PAYMENTSUM'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Total reservas efectuadas hasta el momento' ).
+      WHEN 'SEATSMAX_B'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Ocupación máxima en clase Business' ).
+
+      WHEN 'SEATSOCC_B'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Plazas ocupada en clase Business' ).
+      WHEN 'SEATSMAX_F'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Ocupación máxima en primera clase' ).
+      WHEN 'SEATSOCC_F'.
+        ls_column-r_column->set_visible( if_wdl_core=>visibility_visible ).
+        lo_column_header = ls_column-r_column->get_header( ).
+        lo_column_header->set_ddic_binding_field( if_salv_wd_c_column_settings=>ddic_bind_none ).
+        lo_column_header->set_text( 'Plazas ocupadas en primera clase' ).
+
+    ENDCASE.
+  ENDLOOP.
+
+ENDMETHOD.
